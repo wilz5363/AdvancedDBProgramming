e365f17a24ad3a8fb16b2b2556748126b99748e3
wilz5363 <chanwilson1993@gmail.com>
2016-04-04 00:04:08 +0800

    second car undone commit

--- car.php
+++ car.php
@@ -1,7 +1,9 @@
 <?php
 $section = 'car';
 include 'inc\head.php';
-$data;
+$get_all = 'select c.car_id, c.car_plate, c.car_model, c.car_color, c.car_status, cc.cc_name from car c, CAR_CATEGORY cc where c.CAR_CATEGORY = cc.CC_ID';
+$show_all = $conn->query($get_all)->fetchAll(PDO::FETCH_ASSOC);
+
 $query = 'select * from car_category';
 $result = $conn->query($query)->fetchAll(PDO::FETCH_ASSOC);
 
@@ -14,14 +16,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 //    var_dump($plateno, $model, $color, $category);
 
     $stmt = $conn->prepare('begin insert_car_proc(?,?,?,?); end;');
-    try{
+    try {
         $stmt->bindParam(1, $plateno);
         $stmt->bindParam(2, $model);
         $stmt->bindParam(3, $color);
         $stmt->bindParam(4, $category);
         $exe = $stmt->execute();
-    }catch(PDOException $e){
-        if($e->errorInfo[1] == '1'){
+    } catch (PDOException $e) {
+        if ($e->errorInfo[1] == '1') {
             echo 'Duplicated Car Inserted';
         }
     }
@@ -30,11 +32,37 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 
 ?>
+<div class="container">
+    <h1>Car</h1>
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th>Car ID</th>
+            <th>Plate No</th>
+            <th>Model</th>
+            <th>Color</th>
+            <th>Status</th>
+            <th>Category</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($show_all as $car) { ?>
+            <tr>
+                <td><?php echo $car['CAR_ID'];?></td>
+                <td><?php echo $car['CAR_PLATE'];?></td>
+                <td><?php echo $car['CAR_MODEL'];?></td>
+                <td><?php echo $car['CAR_COLOR'];?></td>
+                <td><?php echo $car['CAR_STATUS'];?></td>
+                <td><?php echo $car['CC_NAME'];?></td>
+                <td><a href="car_edit.php?id=<?php echo  $car['CAR_ID']; ?>">Edit</a></td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+</div>
 
-<h1>Car</h1>
-
-
-<a class="btn btn-primary" data-toggle="modal" href="#modal-id" style="bottom:5%; right: 5%;position: fixed; border-radius: 50%; font-size: 45px; width:70px;height:70px;text-align: center;line-height:60px">+</a>
+<a class="btn btn-primary" data-toggle="modal" href="#modal-id"
+   style="bottom:5%; right: 5%;position: fixed; border-radius: 50%; font-size: 45px; width:70px;height:70px;text-align: center;line-height:60px">+</a>
 <div class="modal fade" id="modal-id">
     <div class="modal-dialog">
         <div class="modal-content">
